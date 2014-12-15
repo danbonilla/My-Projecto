5a5a7afe6ded5bba2ec07c6faf73131026bfe9b9
danbonilla <dbonilla11@gmail.com>
2014-12-15 17:44:50 -0500

    math logic added
    completed logic for project controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,10 +1,25 @@
 class ProjectsController < ApplicationController
   def new
+    @project = Project.new
   end
 
   def create
+    @project = Project.new(project_params)
+
+    if @project.save
+      # @project.total_material_cost
+      redirect_to @project
+    end
+
   end
 
   def show
+    @project = Project.find(params[:id])
+  end
+
+    private
+
+  def project_params
+    params.require(:project).permit(:wall_height, :wall_perimiter, :area_to_exclude_height, :area_to_exclude_perimiter, :sheet_height, :sheet_width, :sheet_cost, :total_sqft, :sheets_needed, :material_cost)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
   get 'welcome/show'
-
-  get '/new' => 'projects#new'
-
-  get 'projects/create'
-
-  get 'projects/show'
-
-  
+  resources :projects
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
