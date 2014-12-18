8d1d59a2d3c67589ed522069b31fffb7ceb97307
danbonilla <dbonilla11@gmail.com>
2014-12-17 22:37:16 -0500

    added some fonts
    added FontSpace fonts to asset pipeline

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,8 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
+
 module MyProjecto
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
